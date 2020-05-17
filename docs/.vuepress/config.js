f701760159752c8832d23a0c3f254fe48b525374
Walk-To-Me <453202851@qq.com>
2020-05-17 13:55:24 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,100 +1,84 @@
 module.exports = {
-  title: "IDocker",
-  description: "MyBlog",
-  dest: "public",
-  head: [
+  "title": "IDocker",
+  "description": "my-blog",
+  "dest": "public",
+  "head": [
     [
       "link",
       {
-        rel: "icon",
-        href: "/favicon.ico",
-      },
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
     ],
     [
       "meta",
       {
-        name: "viewport",
-        content: "width=device-width,initial-scale=1,user-scalable=no",
-      },
-    ],
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
   ],
-  theme: "reco",
-  themeConfig: {
-    nav: [
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
       {
-        text: "Home",
-        link: "/",
-        icon: "reco-home",
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
       },
       {
-        text: "TimeLine",
-        link: "/timeline/",
-        icon: "reco-date",
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
       },
       {
-        text: "Contact",
-        icon: "reco-message",
-        items: [
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
           {
-            text: "GitHub",
-            link: "https://github.com/recoluan",
-            icon: "reco-github",
-          },
-        ],
-      },
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          }
+        ]
+      }
     ],
-    type: "blog",
-    blogConfig: {
-      category: {
-        location: 2,
-        text: "Category",
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
       },
-      tag: {
-        location: 3,
-        text: "Tag",
-      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
     },
-    friendLink: [
+    "friendLink": [
       {
-        title: "午后南杂",
-        desc: "Enjoy when you can, and endure when you must.",
-        email: "1156743527@qq.com",
-        link: "https://www.recoluan.com",
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
       },
       {
-        title: "vuepress-theme-reco",
-        desc: "A simple and beautiful vuepress Blog & Doc theme.",
-        avatar:
-          "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        link: "https://vuepress-theme-reco.recoluan.com",
-      },
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
     ],
-    logo: "/logo.png",
-    search: true,
-    searchMaxSuggestions: 10,
-    sidebar: "auto",
-    lastUpdated: "Last Updated",
-    author: "Air_Code",
-    authorAvatar: "/avatar.png",
-    record: "xxxx",
-    startYear: "2017",
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "sidebar": "auto",
+    "lastUpdated": "Last Updated",
+    "author": "Air_Code",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2017"
   },
-  markdown: {
-    lineNumbers: true,
-  },
-  plugins: [
-    [
-      "@vuepress-reco/vuepress-plugin-kan-ban-niang",
-      {
-        theme: ["blackCat"],
-        clean: false,
-        messages: {
-          welcome: "我是lookroot欢迎你的关注 ",
-          home: "心里的花，我想要带你回家。",
-          theme: "好吧，希望你能喜欢我的其他小伙伴。",
-          close: "再见哦",
-        },
-      },
-    ],
-  ],
-};
+  "markdown": {
+    "lineNumbers": true
+  }
+}
